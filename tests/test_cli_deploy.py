a77afe79d12f07b6b1388040cf4eeb2a1be117e7
Patrick Arminio <patrick.arminio@gmail.com>
2025-06-26 10:24:23 +0100

    ✨ Show dashboard URL when app is deployed

--- tests/test_cli_deploy.py
+++ tests/test_cli_deploy.py
@@ -377,7 +377,7 @@ def test_exits_successfully_when_deployment_is_done(
 
 
 @pytest.mark.respx(base_url=settings.base_api_url)
-def test_exists_successfully_when_deployment_is_done_when_app_is_configured(
+def test_exits_successfully_when_deployment_is_done_when_app_is_configured(
     logged_in_cli: None, tmp_path: Path, respx_mock: respx.MockRouter
 ) -> None:
     app_data = _get_random_app()
@@ -412,7 +412,8 @@ def test_exists_successfully_when_deployment_is_done_when_app_is_configured(
         return_value=Response(
             200,
             json={
-                "message": "Hello, world!",
+                "message": "All good!",
+                "type": "complete",
             },
         )
     )
@@ -426,7 +427,70 @@ def test_exists_successfully_when_deployment_is_done_when_app_is_configured(
 
         assert result.exit_code == 0
 
-        # TODO: show a message when the deployment is done (based on the status)
+        # check that logs are shown
+        assert "All good!" in result.output
+
+        # check that the dashboard URL is shown
+        assert "You can also check the app logs at" in result.output
+        assert deployment_data["dashboard_url"] in result.output
+
+        # check that the app URL is shown
+        assert deployment_data["url"] in result.output
+
+
+@pytest.mark.respx(base_url=settings.base_api_url)
+def test_exits_with_error_when_deployment_fails_to_build(
+    logged_in_cli: None, tmp_path: Path, respx_mock: respx.MockRouter
+) -> None:
+    app_data = _get_random_app()
+    team_data = _get_random_team()
+    app_id = app_data["id"]
+    team_id = team_data["id"]
+    deployment_data = _get_random_deployment(app_id=app_id)
+
+    config_path = tmp_path / ".fastapicloud" / "cloud.json"
+
+    config_path.parent.mkdir(parents=True, exist_ok=True)
+    config_path.write_text(f'{{"app_id": "{app_id}", "team_id": "{team_id}"}}')
+
+    respx_mock.get(f"/apps/{app_id}").mock(return_value=Response(200, json=app_data))
+
+    respx_mock.post(f"/apps/{app_id}/deployments/").mock(
+        return_value=Response(201, json=deployment_data)
+    )
+
+    respx_mock.post(f"/deployments/{deployment_data['id']}/upload").mock(
+        return_value=Response(
+            200,
+            json={"url": "http://test.com", "fields": {"key": "value"}},
+        )
+    )
+
+    respx_mock.post("http://test.com", data={"key": "value"}).mock(
+        return_value=Response(200)
+    )
+
+    respx_mock.get(f"/deployments/{deployment_data['id']}/build-logs").mock(
+        return_value=Response(
+            200,
+            json={
+                "message": "Build failed",
+                "type": "failed",
+            },
+        )
+    )
+
+    respx_mock.post(
+        f"/deployments/{deployment_data['id']}/upload-complete",
+    ).mock(return_value=Response(200))
+
+    with changing_dir(tmp_path):
+        result = runner.invoke(app, ["deploy"])
+
+        assert result.exit_code == 1
+
+        assert "Oh no! Something went wrong" in result.output
+        assert deployment_data["dashboard_url"] in result.output
 
 
 @pytest.mark.respx(base_url=settings.base_api_url)
