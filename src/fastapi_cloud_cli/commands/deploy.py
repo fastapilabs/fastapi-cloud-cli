ee93e04a3e2d0cc822b6b6215acc1b900782ae04
Jonathan Ehwald <github@ehwald.info>
2025-11-28 22:23:36 +0100

    ⚡️ Compress code archives to speed up uploads (#125)

--- src/fastapi_cloud_cli/commands/deploy.py
+++ src/fastapi_cloud_cli/commands/deploy.py
@@ -68,7 +68,7 @@ def archive(path: Path, tar_path: Path) -> Path:
     logger.debug("Archive will be created at: %s", tar_path)
 
     file_count = 0
-    with fastar.open(tar_path, "w") as tar:
+    with fastar.open(tar_path, "w:zst") as tar:
         for filename in files:
             if filename.is_dir():
                 continue
