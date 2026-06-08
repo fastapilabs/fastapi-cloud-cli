47f8f14d13c415e3560103dd8596fee790329170
Patrick Arminio <patrick.arminio@gmail.com>
2026-06-08 11:09:45 +0100

    ✨ Add app create command (#221)

--- src/fastapi_cloud_cli/commands/apps/__init__.py
+++ src/fastapi_cloud_cli/commands/apps/__init__.py
@@ -1,9 +1,11 @@
 import typer
 
+from fastapi_cloud_cli.commands.apps.create import create_app
 from fastapi_cloud_cli.commands.apps.get import get_app
 from fastapi_cloud_cli.commands.apps.list import list_apps
 
 apps_app = typer.Typer(no_args_is_help=True)
+apps_app.command("create")(create_app)
 apps_app.command("get")(get_app)
 apps_app.command("list")(list_apps)
 
