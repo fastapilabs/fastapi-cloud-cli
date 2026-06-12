7c8069db6657a506e368715158548a036cced453
ApurveKaranwal <apurvekaranwal282@gmail.com>
2026-06-12 14:48:24 +0530

    test: add test coverage for new json outputs

--- tests/test_cli_logout.py
+++ tests/test_cli_logout.py
@@ -1,3 +1,4 @@
+import json
 from pathlib import Path
 
 from typer.testing import CliRunner
@@ -27,5 +28,18 @@ def test_logout_with_no_auth_file(temp_auth_config: Path) -> None:
 
     assert result.exit_code == 0
     assert "You are now logged out!" in result.output
+
+    assert not temp_auth_config.exists()
+
+
+def test_logout_json_output(temp_auth_config: Path) -> None:
+    temp_auth_config.write_text('{"access_token": "test_token"}')
+
+    assert temp_auth_config.exists()
+
+    result = runner.invoke(app, ["logout", "--json"])
+
+    assert result.exit_code == 0
+    assert json.loads(result.output) == {"data": {"logged_out": True}}
 
     assert not temp_auth_config.exists()
