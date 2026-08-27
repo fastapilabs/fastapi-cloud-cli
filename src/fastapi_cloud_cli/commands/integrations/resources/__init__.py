ab0249ea10642552ec516ce12ee13b636832d305
Patrick Arminio <patrick.arminio@gmail.com>
2026-08-27 16:17:32 +0200

    ✨ Add `fastapi cloud integrations resources disconnect`
    Shortcake-Parent: main

--- src/fastapi_cloud_cli/commands/integrations/resources/__init__.py
+++ src/fastapi_cloud_cli/commands/integrations/resources/__init__.py
@@ -1,6 +1,9 @@
 import typer
 
 from fastapi_cloud_cli.commands.integrations.resources.connect import connect_resource
+from fastapi_cloud_cli.commands.integrations.resources.disconnect import (
+    disconnect_resource,
+)
 from fastapi_cloud_cli.commands.integrations.resources.get import get_resource
 from fastapi_cloud_cli.commands.integrations.resources.list import list_resources
 
@@ -9,6 +12,7 @@ resources_app = typer.Typer(
     help="Manage resources connected to an app.",
 )
 resources_app.command("connect")(connect_resource)
+resources_app.command("disconnect")(disconnect_resource)
 resources_app.command("get")(get_resource)
 resources_app.command("list")(list_resources)
 
